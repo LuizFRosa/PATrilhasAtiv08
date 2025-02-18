5e97ce56507f210612d168a8dbabc98b0a222bd0
Luiz Filipe Da Rosa <169152129+LuizFRosa@users.noreply.github.com>
2025-02-18 08:01:53 -0300

    Create J-lista 11.cs

---
+++ J-lista 11.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static int CalcularMenor(List<int> numeros)
+    {
+        if (numeros.Count == 0)  
+        {
+            throw new ArgumentException("A lista não pode estar vazia.");
+        }
+        return numeros.Min();  
+    }
+
+    public static void Main()
+    {
+        List<int> numeros = new List<int> { 10, 20, 30, 40, 50 };
+        int menorNumero = CalcularMenor(numeros);
+
+        Console.WriteLine("O menor número é: " + menorNumero);
+        Console.WriteLine("Pressione qualquer tecla para encerrar...");
+        Console.ReadLine();
+    }
+}
